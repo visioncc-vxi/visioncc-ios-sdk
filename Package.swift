e08bdbdafa339d3836d51bf4df431eba5b858510
unpeng <wangp1982@gmail.com>
2024-02-23 16:33:20 +0800

    初始化package

--- Package.swift
+++ Package.swift
@@ -5,23 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "VisionCCLibrary",
+    defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VisionCCLibrary",
-            targets: ["VisionCCLibraryTarget"]),
+            targets: ["VisionCCiOSSDK"]),
     ],
-
+    dependencies: [
+        .package(name: "socket.io-client-swift", url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "16.1.0")),
+        .package(name: "JXSegmentedView", url: "https://github.com/pujiaxin33/JXSegmentedView.git", .upToNextMinor(from: "1.2.7")),
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.7.1")),
+        .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")),
+        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
+        .package(name: "realm-swift", url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.47.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "VisionCCLibraryTarget",
-            path: "VisionCCiOSSDK",
-            resources: [.copy("VisionCCiOSSDK.bundle")],
-            linkerSettings: [
-                .linkedLibrary("z"),
-                .linkedLibrary("c++")
-        ]),
-    ]
+        //.binaryTarget(name: "VisionCCiOSSDK", path: "VisionCCiOSSDK.xcframework"),
+        .binaryTarget(
+            name: "VisionCCiOSSDK", 
+            url: "https://vcc-sdk.vxish.cn/VisionCCiOSSDK.xcframework.zip",
+            checksum: "fc15cdfb1a4ae662240c989a907fd8347cff506e3cbe5376d20b407e34c6fdca"
+        ),
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]    
 )
