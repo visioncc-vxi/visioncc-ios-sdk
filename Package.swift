3539709877febca75b3048a18306b845e7c6fb24
unpeng <wangp1982@gmail.com>
2024-02-23 10:01:57 +0800

    del

--- Package.swift
+++ Package.swift
@@ -11,27 +11,17 @@ let package = Package(
             name: "VisionCCLibrary",
             targets: ["VisionCCLibraryTarget"]),
     ],
-    dependencies: [
-        .package(name: "socket.io-client-swift", url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "16.1.0")),
-        .package(name: "JXSegmentedView", url: "https://github.com/pujiaxin33/JXSegmentedView.git", .upToNextMinor(from: "1.2.7")),
-        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.7.1")),
-        .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")),
-        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
-        .package(name: "realm-swift", url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.47.0")),
-    ],
 
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(
-            name: "VisionCCiOSSDK",
-            url: "https://www.loopns.top/VisionCCiOSSDK.xcframework.zip",
-            checksum: "fc15cdfb1a4ae662240c989a907fd8347cff506e3cbe5376d20b407e34c6fdca"
-        ),
         .target(
             name: "VisionCCLibraryTarget",
-            dependencies: ["socket.io-client-swift", "JXSegmentedView" ,"Alamofire" ,"RxSwift" ,"SnapKit", "realm-swift"],
-        ),
-
+            path: "VisionCCiOSSDK",
+            resources: [.copy("VisionCCiOSSDK.bundle")],
+            linkerSettings: [
+                .linkedLibrary("z"),
+                .linkedLibrary("c++")
+        ]),
     ]
 )
