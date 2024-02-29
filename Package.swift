b210aeda2cc65ac71a63bf39b985163b158e0b20
unpeng <wangp1982@gmail.com>
2024-02-29 20:01:50 +0800

    update spm package

--- Package.swift
+++ Package.swift
@@ -1,36 +1,53 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+ 
 import PackageDescription
-
+ 
 let package = Package(
     name: "VisionCCLibrary",
     defaultLocalization: "en",
+    // 支持的平台和版本
+    platforms: [
+      .iOS(.v12)
+    ],    
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "VisionCCLibrary",
+            name: "VisionCCLibraryTarget",
             targets: ["VisionCCiOSSDK"]),
     ],
     dependencies: [
-        .package(name: "socket.io-client-swift", url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "16.1.0")),
-        .package(name: "JXSegmentedView", url: "https://github.com/pujiaxin33/JXSegmentedView.git", .upToNextMinor(from: "1.2.7")),
-        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.7.1")),
-        .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")),
-        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
-        .package(name: "realm-swift", url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.47.0")),
+        .package(url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "16.1.0")),
+        .package(url: "https://github.com/pujiaxin33/JXSegmentedView.git", .upToNextMinor(from: "1.2.7")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.7.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
+        .package(url: "https://github.com/realm/realm-swift.git", exact: "10.32.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        //.binaryTarget(name: "VisionCCiOSSDK", path: "VisionCCiOSSDK.xcframework"),
-        .binaryTarget(
+        .target(
+            name: "VisionCCLibraryTarget",
+            dependencies: [
+                "VisionCCiOSSDK",
+                .product(name: "SocketIO", package: "socket.io-client-swift"),
+                .product(name: "JXSegmentedView", package: "JXSegmentedView"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "RealmSwift", package: "realm-swift"),
+            ],
+            path: "VisionCCLibrary"
+        ),
+       .binaryTarget(
             name: "VisionCCiOSSDK", 
-            url: "https://vcc-sdk.vxish.cn/VisionCCiOSSDK-1.0.7.xcframework.zip",
-            checksum: "5a4d8ab11f47f18d94bc12b38f26b69dea428334d55713a6b1dcebf7124630b7"
-        )
+            url: "https://vcc-sdk.vxish.cn/sdk/1.1.0/VisionCCiOSSDK.xcframework.zip",
+            checksum: "9fba2e41abc4a5db51b11bf7c861c068dd1aea1612972b08b30ba0010d61bc52"
+        ),
     ],
     swiftLanguageVersions: [
         .v5
-    ]    
+    ]  
 )
