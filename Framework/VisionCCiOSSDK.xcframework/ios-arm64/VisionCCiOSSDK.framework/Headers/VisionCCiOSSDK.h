9d875033b108f2d5b4070250dd5ce54dde02a3a0
unpeng <wangp1982@gmail.com>
2024-05-31 18:18:21 +0800

    update wk 2.0.0

---
+++ Framework/VisionCCiOSSDK.xcframework/ios-arm64/VisionCCiOSSDK.framework/Headers/VisionCCiOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  VisionCCiOSSDK.h
+//  VisionCCiOSSDK
+//
+//  Created by 王鹏 on 2024/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisionCCiOSSDK.
+FOUNDATION_EXPORT double VisionCCiOSSDKVersionNumber;
+
+//! Project version string for VisionCCiOSSDK.
+FOUNDATION_EXPORT const unsigned char VisionCCiOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisionCCiOSSDK/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -2,49 +2,12 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
  
 import PackageDescription
-import Foundation
-
-func runCommand() -> String {
-    let task = Process()
-    let pipe = Pipe()
-    let base = "15.0.1"
-    task.executableURL = URL(fileURLWithPath: "/usr/bin/xcodebuild")
-    task.arguments = ["-version"]
-    task.standardInput = nil
-    task.standardError = nil
-    task.standardOutput = pipe
-
-    do {
-        try task.run()
-    } catch {
-        return base
-    }
-
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = String(data: data, encoding: .utf8) ?? ""
-    let range = NSRange(output.startIndex..., in: output)
-
-    let pattern = #"(\d+\.\d+(\.\d+)?(\.\d+)?)\b"#
-    guard let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive),
-          let firstMatch = regex.matches(in: output, range: range).first else {
-        return base
-    }
-
-    let versionRange = firstMatch.range(at: 1)
-    if let range = Range(versionRange, in: output) {
-        let version = output[range]
-        return String(version)
-    } else {
-        return base
-    }
-}
 
 extension Target {
     static func sdk() -> Target {
-        let result = runCommand()
         return .binaryTarget(
                 name: "VisionCCiOSSDK", 
-                path: "Framework/\(result)/VisionCCiOSSDK.xcframework"
+                path: "Framework/VisionCCiOSSDK.xcframework"
             )
     }
 }
@@ -62,26 +25,13 @@ let package = Package(
             name: "VisionCCLibraryTarget",
             targets: ["VisionCCLibraryTarget"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "16.1.0")),
-        .package(url: "https://github.com/pujiaxin33/JXSegmentedView.git", .upToNextMinor(from: "1.2.7")),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.7.1")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0"))
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "VisionCCLibraryTarget",
             dependencies: [
-                "VisionCCiOSSDK",
-                .product(name: "SocketIO", package: "socket.io-client-swift"),
-                .product(name: "JXSegmentedView", package: "JXSegmentedView"),
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "SnapKit", package: "SnapKit"),
-                .product(name: "RxSwift", package: "RxSwift"),
-                .product(name: "RxCocoa", package: "RxSwift")
+                "VisionCCiOSSDK"
             ],
             path: "VisionCCLibrary"
         ),
