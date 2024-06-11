330730aff62c8abde6e95c50ddf2c676dd0c1ef3
unpeng <wangp1982@gmail.com>
2024-06-11 20:13:57 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,16 +26,7 @@ let package = Package(
             targets: ["VisionCCLibraryTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "16.1.0")),
-        .package(url: "https://github.com/pujiaxin33/JXSegmentedView.git", .upToNextMinor(from: "1.2.7")),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.7.1")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
-        .package(url: "https://github.com/longitachi/ZLPhotoBrowser.git", .upToNextMajor(from: "4.5.3")),
-        .package(url: "https://github.com/mchoe/SwiftSVG.git", .upToNextMajor(from: "2.3.2")),
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", exact: "6.5.11"),
-        .package(url: "https://github.com/RxSwiftCommunity/NSObject-Rx.git", .upToNextMajor(from: "5.2.2")),
-        .package(url: "https://github.com/unpeng/MJRefreshSwift.git", .upToNextMajor(from: "1.5.6")),
+        .package(url: "https://github.com/longitachi/ZLPhotoBrowser.git", .upToNextMajor(from: "4.5.3"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -44,16 +35,7 @@ let package = Package(
             name: "VisionCCLibraryTarget",
             dependencies: [
                 "VisionCCiOSSDK",
-                .product(name: "SocketIO", package: "socket.io-client-swift"),
-                .product(name: "JXSegmentedView", package: "JXSegmentedView"),
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "SnapKit", package: "SnapKit"),
-                .product(name: "RxSwift", package: "RxSwift"),
-                .product(name: "ZLPhotoBrowser", package: "ZLPhotoBrowser"),
-                .product(name: "IQKeyboardManagerSwift", package: "IQKeyboardManager"),
-                .product(name: "SwiftSVG", package: "SwiftSVG"),
-                .product(name: "MJRefreshSwift", package: "MJRefreshSwift"),
-                .product(name: "NSObject-Rx", package: "NSObject-Rx"),
+                .product(name: "ZLPhotoBrowser", package: "ZLPhotoBrowser")
             ],
             path: "VisionCCLibrary"
         ),
