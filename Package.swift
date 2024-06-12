efd905a33c37b11dee08af967ac27c695ee86872
unpeng <wangp1982@gmail.com>
2024-06-12 15:15:42 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,30 +6,15 @@ import PackageDescription
 let package = Package(
     name: "VisionCCLibrary",
     defaultLocalization: "en",
-    // 支持的平台和版本
     platforms: [
       .iOS(.v12)
-    ],    
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VisionCCLibrary",
-            targets: ["VisionCCLibraryTarget"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/longitachi/ZLPhotoBrowser.git", .upToNextMajor(from: "4.5.3")),
-        .package(url: "https://github.com/unpeng/MJRefreshSwift.git", .upToNextMajor(from: "1.5.6"))
+            targets: ["VisionCCiOSSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "VisionCCLibraryTarget",
-            dependencies: [
-                "VisionCCiOSSDK"
-            ],
-            path: "VisionCCLibrary"
-        ),
         .binaryTarget(
             name: "VisionCCiOSSDK", 
             path: "VisionCCiOSSDK.xcframework"
