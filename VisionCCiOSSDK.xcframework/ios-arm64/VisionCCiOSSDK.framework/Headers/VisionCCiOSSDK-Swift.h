46b79f5007fed6c540fb2b16cb3b0d92df9b130b
unpeng <wangp1982@gmail.com>
2025-07-22 12:25:04 +0800

    2.1.3
    2.1 fixed

--- VisionCCiOSSDK.xcframework/ios-arm64/VisionCCiOSSDK.framework/Headers/VisionCCiOSSDK-Swift.h
+++ VisionCCiOSSDK.xcframework/ios-arm64/VisionCCiOSSDK.framework/Headers/VisionCCiOSSDK-Swift.h
@@ -314,6 +314,7 @@ SWIFT_CLASS("_TtC14VisionCCiOSSDK7CCKFApi")
 - (void)viewWillAppear:(BOOL)animated;
 - (void)viewWillDisappear:(BOOL)animated;
 - (void)viewDidLoad;
+- (void)viewSafeAreaInsetsDidChange;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
